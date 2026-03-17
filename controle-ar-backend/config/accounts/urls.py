c352ee890c31ff9175d8d526d6e9bed748552402
Cleber Henrique <cleberhenriquelacerdaduarte@gmail.com>
2026-03-17 13:49:53 -0300

    /feature/troca de senha e logout implementado

--- controle-ar-backend/config/accounts/urls.py
+++ controle-ar-backend/config/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterUserView, VerifyUserEmail, LoginUserView, TestAuthenticationView
+from .views import RegisterUserView, VerifyUserEmail, LoginUserView, TestAuthenticationView, PasswordResetRequestView, SetNewPassword, PasswordResetConfirm
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='refresh'),
     path('profile/', TestAuthenticationView.as_view(), name='granted'),
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
+    path('password-reset-confirm/<uidb64>/<token>',  PasswordResetConfirm.as_view(), name='password-reset-confirm'),
+    path('set-new-password/',  SetNewPassword.as_view(), name='set-new-password')
 ]
--- controle-ar-backend/config/accounts/views.py
+++ controle-ar-backend/config/accounts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
-from .serializers import UserRegisterSerializer, LoginSerializer
+from .serializers import UserRegisterSerializer, LoginSerializer, PasswordResetRequestSerializer, SetNewPasswordSerializer, LogoutUserSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .utils import send_code_to_email_user
 from .models import OneTimePassord
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import smart_str, DjangoUnicodeDecodeError
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 # Create your views here.
 
 class RegisterUserView(GenericAPIView):
@@ -58,3 +61,38 @@ class TestAuthenticationView(GenericAPIView):
             'msg': 'its works'
         }
         return Response(data, status=status.HTTP_200_OK)
+    
+class PasswordResetRequestView(GenericAPIView):
+    serializer_class=PasswordResetRequestSerializer
+    def post(self, request):
+        serializer=self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        Response({'message': "Um link foi enviado para seu email, para poder mudar de senha."}, status=status.HTTP_200_OK)
+
+class PasswordResetConfirm(GenericAPIView):
+    def get(self, request, uidb64, token):
+        try:
+            user_id=smart_str(urlsafe_base64_decode(uidb64))
+            user=User.objects.get(id=user_id)
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                return Response({'message':"O token é inválido ou foi expirado."}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({'success': True, 'message':"Credenciais Válidas", 'uidb64':uidb64, 'token':token}, status=status.HTTP_200_OK)
+        except DjangoUnicodeDecodeError:
+            return Response({'message':"O token é inválido ou foi expirado."}, status=status.HTTP_401_UNAUTHORIZED)
+
+class SetNewPassword(GenericAPIView):
+    serializer_class=SetNewPasswordSerializer
+    def patch(self, request):
+        serializer=self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response({'message':"Senha mudada com sucesso!"}, status=status.HTTP_200_OK)
+    
+class LogoutUserView(GenericAPIView):
+    serializer_class=LogoutUserSerializer
+    permission_classes=[IsAuthenticated]
+
+    def post(self, request):
+        serializer=self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
